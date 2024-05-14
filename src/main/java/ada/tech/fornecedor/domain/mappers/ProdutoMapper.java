4bed0ae0efaa9250b8891c869c7c65cb7ae4a85c
ramonvm18 <ramon.mastropaulo@hotmail.com>
2024-05-14 15:39:21 -0300

    update ProdutoMapper and Dto

--- src/main/java/ada/tech/fornecedor/domain/mappers/ProdutoMapper.java
+++ src/main/java/ada/tech/fornecedor/domain/mappers/ProdutoMapper.java
@@ -12,10 +12,11 @@ public class ProdutoMapper {
                 .apresentacao(dto.getApresentacao())
                 .lote(dto.getLote())
                 .dataFabricacao(dto.getDataFabricacao())
-
+                .fabricante(dto.getFabricante())
                 .preco(dto.getPreco())
                 .cargaPerigosa(dto.isCargaPerigosa())
                 .volume(dto.getVolume())
+                .imagem(dto.getImagem())
                 .build();
     }
 
@@ -26,9 +27,11 @@ public class ProdutoMapper {
                 .apresentacao(entity.getApresentacao())
                 .lote(entity.getLote())
                 .dataFabricacao(entity.getDataFabricacao())
+                .fabricante(entity.getFabricante())
                 .preco(entity.getPreco())
                 .cargaPerigosa(entity.isCargaPerigosa())
                 .volume(entity.getVolume())
+                .imagem(entity.getImagem())
                 .id(entity.getId())
                 .build();
     }
