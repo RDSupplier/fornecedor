3365943aec4eec79fdb5562a6b3b533c66e7f447
igorosilva <igoroliveirasilva772@gmail.com>
2024-06-20 21:41:01 -0300

    estilizacao email

--- src/main/java/ada/tech/fornecedor/util/EmailUtil.java
+++ src/main/java/ada/tech/fornecedor/util/EmailUtil.java
@@ -18,9 +18,21 @@ public class EmailUtil {
         mimeMessageHelper.setTo(email);
         mimeMessageHelper.setSubject("Código de confirmação");
         mimeMessageHelper.setText("""
-                Olá, seu código de confirmação é: %s
+                <html>
+                <body style="font-family: 'rdmodern-medium', sans-serif; width: 100%%;">
+                    <div style="text-align: center;">
+                        <h2 style="color: #0F8143;">Redefinição de senha</h2>
+                        <p>Olá,</p>
+                        <br>
+                        <p>Você solicitou sua redefinição de senha</p>
+                        <p>Seu código de confirmação é: <span style="font-weight: bold;">%s</span></p>
+                        <br>
+                        <p>Se você não solicitou esta redefinição, por favor ignore este email.</p>
+                        <p>Atenciosamente,<br/>RDSupplier</p>
+                    </div>
+                </body>
+                </html>
                 """.formatted(otp), true);
-
 
         javaMailSender.send(mineMessage);
     }
